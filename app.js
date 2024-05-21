0212acbf61b48800950679770ff2f9a4eeda83ff
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-21 18:25:04 +0900

    JavaScriptだけでリストを追加する

---
+++ app.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+const unorderedList = document.querySelector('ul');
+
+button.addEventListener('click', addGoal);
+
+function addGoal() {
+  const value = input.value;
+  const listItem = document.createElement('li');
+  listItem.textContent = value;
+
+  unorderedList.appendChild(listItem);
+}
